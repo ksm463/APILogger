63d6a77e414a21ad4515daef6c682a6ddb060c6e
sungmin <ksm46351@gmail.com>
2025-03-19 16:11:28 +0900

    aiohttp_ClientSession

--- src/router/api_client.py
+++ src/router/api_client.py
@@ -1,3 +1,4 @@
+import aiohttp
 import json
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import JSONResponse
@@ -19,31 +20,38 @@ async def send_api_request(
     try:
         content_type = request.headers.get("Content-Type", "")
         if "application/json" in content_type:
-            ip = request.client.host
+            client_ip = request.client.host
+            target_url = request.url
             method_val = request.method
             json_data = await request.json()
             log_content = json.dumps(json_data, ensure_ascii=False)
             
         else:
             form = await request.form()
-            ip = request.client.host
-            # ip = form.get("ip")
+            client_ip = request.client.host
+            target_url = form.get("ip")
             method_val = form.get("method")
             log_content = form.get("content", "")
-        
-        # 필수 필드 검증
-        if not ip or not method_val:
-            raise ValueError("필수 필드가 누락되었습니다: ip와 method가 필요합니다.")
 
-        logger.info(f"[/send] 요청 시작: 서버 IP - {ip}, 메서드 - {method_val}")
+        logger.info(f"[/send] 요청 시작: 클라이언트 IP - {client_ip}, 메서드 - {method_val}")
 
         user_agent = request.headers.get("user-agent", "Unknown")
-        
-        # 로그 데이터 생성 시 HTTP 메서드는 request.method 사용
-        log_data = create_log_data(config, logger, db_engine, request.method, user_agent, ip, log_content)
+
+        log_data = create_log_data(config, logger, db_engine, request.method, user_agent, client_ip, log_content)
         print(f"받은 요청: {log_data.method}")
 
-        return JSONResponse(content=log_content)
+        server_ip = config["ADDRESS"]["SERVER_IP_ADDRESS"]
+        headers = {"Content-Type": "application/json"}
+        async with aiohttp.ClientSession() as session:
+            logger.debug(f"[/sendexample] 서버로 POST 요청 전송 시도: URL - http://{server_ip}/testexample, 헤더 - {headers}, 데이터 - {log_content}")
+            async with session.post(
+                f"http://{server_ip}/testexample", headers=headers, json=json_data
+            ) as response:
+                logger.debug(f"[/sendexample] 서버로 POST 요청 전송: {server_ip}, 데이터 - {log_content}")
+                response.raise_for_status()
+                response_data = await response.json()
+                logger.info(f"[/sendexample] 응답 수신 성공: 상태 코드 - {response.status}, 응답 데이터 - {response_data}")
+                return JSONResponse(content=response_data, status_code=response.status)
     except Exception as e:
         logger.exception(f"예외 발생: {e}")
         return JSONResponse(content="서버 내부 오류", status_code=500)
